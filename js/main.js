01aa229dda34747111ce7cf5fcf24c60d656d341
AX99 <rapha.mensah@gmail.com>
2022-01-28 09:32:11 +0000

    form

--- js/main.js
+++ js/main.js
@@ -1,84 +1,119 @@
+$(document).ready( function() {
+    var input = $('.validate-input .input100');
+    var modal = $('#modal');
+    var open = $('.validate-form button');
+    var span = $('.close')
 
-
-
-$('.contact100-form').on('submit',function(e){
-    //optional validation code here
-    
-    (function ($) {
-        "use strict";
-
-        /*==================================================================
-        [ Validate ]*/
-        var input = $('.validate-input .input100');
-
-        $('.validate-form').on('submit',function(){
-            var check = true;
-
-            for(var i=0; i<input.length; i++) {
-                if(validate(input[i]) == false){
-                    showValidate(input[i]);
-                    check=false;
-                }
-            }
-
-            return check;
-        });
-
-
-        $('.validate-form .input100').each(function(){
-            $(this).focus(function(){
-            hideValidate(this);
-            });
-        });
-
-        function validate (input) {
-            if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-                if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                    return false;
-                }
-            }
-            else {
-                if($(input).val().trim() == ''){
-                    return false;
-                }
-            }
-        }
-
-        function showValidate(input) {
-            var thisAlert = $(input).parent();
-
-            $(thisAlert).addClass('alert-validate');
-        }
-
-        function hideValidate(input) {
-            var thisAlert = $(input).parent();
-
-            $(thisAlert).removeClass('alert-validate');
-        }
-
-    })(jQuery);
-
-    // e.preventDefault();
-  
-    $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbwGZiICFKsrYn3i5qOmRLCEX_yVejyywcwd88fcED2aTTMjJjHSkjQFvRxp44FO7u93/exec",
-        method: "POST",
-        dataType: "json",
-        data: $(".contact100-form").serialize(),
-        success: function(response) {
-            
-            if(response.result == "success") {
-                $('.contact100-form')[0].reset();
-                alert('Thank you for contacting us.');
-                return true;
-            }
-            else {
-                alert("Something went wrong. Please try again.")
-            }
-        },
-        error: function() {
-            
-            alert("Something went wrong. Please try again.")
-        }
+    open.click(function(e){
+        e.preventDefault()
+        modal.show();
+        $('#mce-EMAIL').val(input.val())
     })
+
+    span.click(function(){
+        $(modal).hide()
+    })
+
+    if ($(modal).is(":visible")){
+        $('#modal :not(.form-popup)').click(function() {
+        $(modal).toggle();
+     });
+    }
+    
+    
+    
+
 });
+
+
+
+
+
+
+
+// (function ($) {
+//     "use strict";
+
+//     /*==================================================================
+//     [ Validate ]*/
+//     var input = $('.validate-input .input100');
+
+//     $('.validate-form').on('submit',function(){
+//         var check = true;
+
+//         for(var i=0; i<input.length; i++) {
+//             if(validate(input[i]) == false){
+//                 showValidate(input[i]);
+//                 check=false;
+//             }
+//         }
+
+//         return check;
+//     });
+
+
+//     $('.validate-form .input100').each(function(){
+//         $(this).focus(function(){
+//            hideValidate(this);
+//         });
+//     });
+
+//     function validate (input) {
+//         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//                 return false;
+//             }
+//         }
+//         else {
+//             if($(input).val().trim() == ''){
+//                 return false;
+//             }
+//         }
+//     }
+
+//     function showValidate(input) {
+//         var thisAlert = $(input).parent();
+
+//         $(thisAlert).addClass('alert-validate');
+//     }
+
+//     function hideValidate(input) {
+//         var thisAlert = $(input).parent();
+
+//         $(thisAlert).removeClass('alert-validate');
+//     }
+
+    
+    
+
+// })(jQuery);
+
+// $('.contact100-form').on('submit',function(e){
+//     optional validation code here
+    
+    
+
+//     e.preventDefault();
+  
+//     $.ajax({
+//         url: "https://script.google.com/macros/s/AKfycbwGZiICFKsrYn3i5qOmRLCEX_yVejyywcwd88fcED2aTTMjJjHSkjQFvRxp44FO7u93/exec",
+//         method: "POST",
+//         dataType: "json",
+//         data: $(".contact100-form").serialize(),
+//         success: function(response) {
+            
+//             if(response.result == "success") {
+//                 $('.contact100-form')[0].reset();
+//                 alert('Thank you for contacting us.');
+//                 return true;
+//             }
+//             else {
+//                 alert("Something went wrong. Please try again.")
+//             }
+//         },
+//         error: function() {
+            
+//             alert("Something went wrong. Please try again.")
+//         }
+//     })
+// });
